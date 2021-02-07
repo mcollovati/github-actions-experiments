aaceedf38d0fb7225855d79f2a269a77d9297747
Marco Collovati <mcollovati@gmail.com>
2021-02-07 17:46:38 +0100

    Test project

---
+++ src/main/java/com/mcollovati/github/spikes/App.java
@@ -0,0 +1,13 @@
+package com.mcollovati.github.spikes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
